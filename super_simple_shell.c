24c962ee582f4ec9dd23b464f0b6ff4091935b4e
Akatsuki667 <qextzy@gmail.com>
2024-12-18 18:53:41 +0100

    change code

--- super_simple_shell.c
+++ super_simple_shell.c
@@ -25,35 +25,40 @@ int main()
 		if (getline(&line, &len, stdin) == - 1) /* vérification lecture a échoué */
 		/* lire ligne d'entrée à partir de stdin(clavier) */
 		{
-			perror("Error detected, exiting\n");
-			free(line);
-			return (1);
+			perror("Error: Unable to read input");
+			free(line); /* libération mémoire alloué à line par getline() */
+			return (1); /* indication erreur */
 		}
 
 		token = strtok(line, delim); /* découpage line en sous chaîne */
-		while (token) /* tant que mots disponible */
+		while (token) /* tant que mots à extraire */
 		{
 			array[i++] = token; /* stockage de chaque mot trouvé */
 			token = strtok(NULL, delim); /* appel pour continuer analyse line */
 		}
+		array[i] = NULL; /* ajout NULL élément -> execve sait 
+								où s'arrète liste d'arguments */
 
 		pid = fork(); /* création processus enfant */
 		if (pid == - 1) /* vérification échec processus */
 		{
-			perror("Error:");
-			return (1);
+			perror("Error: Fork failled");
+			free(line); /* libération mémoire alloué à line par getline() */
+			return (1); /* indication erreur */
 		}
 		else if (pid == 0) /* vérification processus enfant */
 		{
 			if (execve(array[0], array, NULL) == -1)
 			/* vérification exécution valide */
-				perror("Error:");
+				perror("Error: Command execution failed");
+				free(line); /* libération mémoire alloué à line par getline() */
+				exit(EXIT_FAILURE); /* termine processus enfant avec erreur */
 		}
 		else /* Sinon processus parent */
 		{
 			wait(&status); /* instruction pour ce dernier */
 		}
 	}	
-	free(line);
+	free(line); /* libération mémoire alloué à line par getline() */
 	return (0);
 }
